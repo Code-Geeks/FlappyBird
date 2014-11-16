5353565f0ae8e7ac68421a4b08ef482919562f1f
Vijay Chavda <chavdavijay@outlook.in>
2014-11-16 20:13:38 +0530

    Introduced Birdy

---
+++ FlappyBird/Birdy.xaml.h
@@ -0,0 +1,18 @@
+﻿//
+// Birdy.xaml.h
+// Declaration of the Birdy class
+//
+
+#pragma once
+
+#include "Birdy.g.h"
+
+namespace FlappyBird
+{
+	[Windows::Foundation::Metadata::WebHostHidden]
+	public ref class Birdy sealed
+	{
+	public:
+		Birdy();
+	};
+}
