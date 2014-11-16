fac1cdd27569a9171f55badbb98be2456f1abfd1
Vijay Chavda <chavdavijay@outlook.in>
2014-11-17 05:00:03 +0530

    Introduced IncommingThing object.

---
+++ FlappyBird/IncommingThing.xaml.cpp
@@ -0,0 +1,54 @@
+﻿//
+// IncommingThing.xaml.cpp
+// Implementation of the IncommingThing class
+//
+
+#include "pch.h"
+#include "IncommingThing.xaml.h"
+
+using namespace FlappyBird;
+
+using namespace Platform;
+using namespace Windows::Foundation;
+using namespace Windows::Foundation::Collections;
+using namespace Windows::UI::Xaml;
+using namespace Windows::UI::Xaml::Controls;
+using namespace Windows::UI::Xaml::Controls::Primitives;
+using namespace Windows::UI::Xaml::Data;
+using namespace Windows::UI::Xaml::Input;
+using namespace Windows::UI::Xaml::Media;
+using namespace Windows::UI::Xaml::Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+IncommingThing::IncommingThing()
+{
+	InitializeComponent();
+
+	auto current = Window::Current;
+	auto width = current->Bounds.Width;
+
+	Left = width;
+	Speed = 3;
+
+	Life();
+}
+
+void IncommingThing::Life()
+{
+	auto timer = ref new DispatcherTimer();
+	timer->Tick +=ref new Windows::Foundation::EventHandler<Platform::Object ^>(this, &FlappyBird::IncommingThing::OnTick);
+	timer->Start();
+}
+
+void IncommingThing::OnTick(Object ^sender, Object ^args)
+{
+	auto currentWindow = Window::Current;
+	auto bounds = currentWindow->Bounds;
+
+	auto minLeft = -Width * 1.5;
+	auto thingLeft = Canvas::GetLeft(this);
+
+	if (thingLeft > minLeft)
+		Left -= Speed;
+}
---
+++ FlappyBird/IncommingThing.xaml.h
@@ -0,0 +1,66 @@
+﻿//
+// IncommingThing.xaml.h
+// Declaration of the IncommingThing class
+//
+
+#pragma once
+
+#include "IncommingThing.g.h"
+
+namespace FlappyBird
+{
+	[Windows::Foundation::Metadata::WebHostHidden]
+	public ref class IncommingThing sealed
+	{
+	internal:
+		void Life();
+
+	public:
+		property double Left
+		{
+			double get()
+			{
+				return _left;
+			}
+			void set(double value)
+			{
+				//manage intersections
+				Canvas::SetLeft(this, value);
+				_left = value;
+			}
+		}
+
+		property double Top
+		{
+			double get()
+			{
+				return _right;
+			}
+			void set(double value)
+			{
+				//manage intersections
+				Canvas::SetTop(this, value);
+				_right = value;
+			}
+		}
+
+		property double Speed
+		{
+			double get()
+			{
+				return _speed;
+			}
+
+			void set(double value)
+			{
+				_speed = value;
+			}
+		}
+
+		IncommingThing();
+
+	private:
+		double _left, _right, _speed;
+		void OnTick(Platform::Object ^sender, Platform::Object ^args);
+	};
+}
